286c7538d911d6c72f55ad1fb519c8295be49a35
Matthew Nessel <mdnessel@bsu.edu>
2019-09-24 11:11:40 -0400

    Add project base files

---
+++ src/module-info.java
@@ -0,0 +1,6 @@
+module BinaryCalculator {
+    requires javafx.fxml;
+    requires javafx.controls;
+
+    opens sample;
+}
---
+++ src/sample/BinaryConvertor.java
@@ -0,0 +1,8 @@
+package sample;
+
+public class BinaryConvertor {
+
+    public int convertBinaryToDecimal(String binary){
+        return 1;
+    }
+}
